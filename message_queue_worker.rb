fe6af747c0b7b9e11a7a1af867e94182735b300e
danielzhangcs <danielzhang@brandeis.edu>
2020-05-05 01:06:11 -0400

    new post tweet

--- message_queue_worker.rb
+++ message_queue_worker.rb
@@ -1,6 +1,7 @@
 require "bunny"
 require 'json'
 require_relative 'helper/test_helper.rb'
+require './models/tweet.rb'
 
 class MQ
   def initialize(redis)
@@ -23,7 +24,22 @@ class MQ
     puts " [*] Waiting for messages in #{@queue.name}. To exit press CTRL+C"
     @queue.subscribe(block: true) do |delivery_info, properties, body|
       content =  JSON.parse(body)
-      TestHelper.generate_tweet(content["user_id"].to_i, content["tweet_cnt"].to_i, @redis)
+      if content["tweet_content"].nil?
+         TestHelper.generate_tweet(content["user_id"].to_i, content["tweet_cnt"].to_i, @redis)
+      else
+        tweet = Tweet.new(user_id: content["user_id"].to_i, content: content["tweet_content"])
+        if tweet.save
+          redis_key = content["user_id"] + "-1"
+          if redis.exists(redis_key)
+              redis.lpush(redis_key, tweet.to_json)
+              redis.rpop(redis_key) if redis.llen(redis) > 10
+
+          end
+
+        end
+
+
+      end
     end
   end
 
