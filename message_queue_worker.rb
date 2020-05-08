f02d988e5d4a56c46da2c84ac6cf6b8ecb3fec13
danielzhangcs <danielzhang@brandeis.edu>
2020-05-08 15:52:46 -0400

    add logic to handle push tweet to followers

--- message_queue_worker.rb
+++ message_queue_worker.rb
@@ -28,10 +28,31 @@ class MQ
          TestHelper.generate_tweet(content["user_id"].to_i, content["tweet_cnt"].to_i)
       else
         tweet = Tweet.new(user_id: content["user_id"].to_i, content: content["tweet_content"])
+
         if tweet.save
+
           redis_key = content["user_id"] + "-1"
+          tweet = ActiveRecord::Base.connection.execute(
+              "select tweets.content, tweets.created_at, tweets.user_id, users.name
+  from tweets inner join users on tweets.user_id = users.id
+  where tweets.id=#{tweet.id}"
+          )[0]
+          tweet["created_at"] = tweet["created_at"].to_s
+
+
           if redis.exists(redis_key)
               redis.sadd(redis_key, tweet.to_json)
+          end
+
+
+          follower_ids = ActiveRecord::Base.connection.execute(
+              "select follows.follower_id from follows where followed_id=#{tweet["user_id"]}").map{|x| x["follower_id"]}
+
+          follower_ids.each  do |id|
+            follower_redis_key = id.to_s + "-1"
+            if redis.exists(follower_redis_key)
+              redis.sadd(follower_redis_key, tweet.to_json)
+            end
           end
 
         end
