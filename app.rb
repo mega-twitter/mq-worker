4704334ab84fd0378b420f1037d15f1cbf30f08d
danielzhangcs <danielzhang@brandeis.edu>
2020-05-11 15:22:53 -0400

    add password for redis

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ require_relative 'helper/test_helper.rb'
 require_relative 'message_queue_worker'
 require 'json'
 
-set :redis, Redis.new(host: "157.245.114.60", port: 6379)
+set :redis, Redis.new(host: "157.245.114.60", port: 6379, password: "foo")
 
 $mq = MQ.new(settings.redis)
 
